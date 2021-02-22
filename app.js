76fe4be3a3330743ccb68d7d10a4af1f5238c3ac
bcdguz <bcdguz96@gmail.com>
2021-02-22 11:22:49 -0800

    Creating a session token for logged in users via passport

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tasks = require("./routes/api/tasks");
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,8 +4,18 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.json({ 
+        id: req.user.id,
+        name: req.user.name,
+        username: req.user.username,
+        email: req.user.email
+    });
+})
 
 router.post('/register', (req, res) => {
     User.findOne({ email: req.body.email })
