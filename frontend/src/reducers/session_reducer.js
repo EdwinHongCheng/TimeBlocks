e04610e2d022977ca21bd571954ef1413479c45d
IsoVoyd <dubnersamuel@gmail.com>
2021-02-22 12:19:27 -0800

    add more cases to session reducer

--- frontend/src/reducers/session_reducer.js
+++ frontend/src/reducers/session_reducer.js
@@ -1,4 +1,7 @@
-import { RECEIVE_USER_LOGOUT } from "../actions/session_actions"
+import { STATES } from "mongoose"
+import { RECEIVE_CURRENT_USER,
+         RECEIVE_USER_LOGOUT,
+         RECEIVE_USER_SIGN_IN } from "../actions/session_actions"
 
 const initialState = {
     isAuthenticated: false,
@@ -9,10 +12,21 @@ const sessionReducer = (prevState=initialState, action) => {
     Object.freeze(prevState)
 
     switch(action.type) {
+        case RECEIVE_CURRENT_USER:
+            return {
+                ...prevState,
+                isAuthenticated: !!action.currentUser,
+                user: action.currentUser
+            }
         case RECEIVE_USER_LOGOUT:
             return {
                 isAuthenticated: false,
                 user: undefined
+            }
+        case RECEIVE_USER_SIGN_IN:
+            return {
+                ...prevState,
+                isSignedIn: true
             }
         default:
             return prevState;
