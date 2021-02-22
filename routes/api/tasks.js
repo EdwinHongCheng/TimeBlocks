21010205a0e082ca212e8c698aa786371ef7ffe6
bcdguz <bcdguz96@gmail.com>
2021-02-22 11:58:41 -0800

    Creating validations for a task

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const Task = require('../../models/Task');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the tasks route" }));
 
