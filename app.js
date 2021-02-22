a97c49459e9aed7461268ca517a24932c5ea03af
bcdguz <bcdguz96@gmail.com>
2021-02-22 10:34:45 -0800

    Creating routes for tasks and users

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tasks = require("./routes/api/tasks");
+const bodyParser = require('body-parser');
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//Backend Routes
 app.get("/", (req, res) => res.send("Wassup World"));
+app.use("/api/users", users);
+app.use("/api/tasks", tasks);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
