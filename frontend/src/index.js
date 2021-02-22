6d0605008a5cd627256519cc75a1956a99b14626
IsoVoyd <dubnersamuel@gmail.com>
2021-02-22 11:10:19 -0800

    add session_actions and session_reducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,37 +1,39 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
 
-import Root from "./components/root"
+import Root from "./components/root";
 
 import configureStore from "./store/store";
 
-import jwt_decode from "jwt-decode"
-import { setAuthToken } from "./util/session_api_util"
+import jwt_decode from "jwt-decode";
+import { setAuthToken } from "./util/session_api_util";
 
-import { logout } from "./actions/session_actions"
+import { logout } from "./actions/session_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
-    let store;
+  let store;
 
-    if (localStores.jwtToken) {
-        setAuthToken(localStorage.jwtToken);
-        const decodedUser = jwt_decode(localStorage.jwtToken)
+  if (localStores.jwtToken) {
+    setAuthToken(localStorage.jwtToken);
+    const decodedUser = jwt_decode(localStorage.jwtToken);
 
-        const preloadedState = { session: { isAuthenticated: true, user: decodedUser } }
+    const preloadedState = {
+      session: { isAuthenticated: true, user: decodedUser },
+    };
 
-        store = configureStore(preloadedState)
+    store = configureStore(preloadedState);
 
-        const currentTime = Date.now / 1000
+    const currentTime = Date.now / 1000;
 
-        if (decodedUser.exp < currentTime) {
-            store.dispatch(logout())
-            window.location.href = "/login"
-        }
-    } else {
-        store = configureStore({})
+    if (decodedUser.exp < currentTime) {
+      store.dispatch(logout());
+      window.location.href = "/login";
     }
+  } else {
+    store = configureStore({});
+  }
 
-    const root = document.getElementById("root")
+  const root = document.getElementById("root");
 
-    ReactDOM.render(<Root store={store} />, root)
-})
+  ReactDOM.render(<Root store={store} />, root);
+});
