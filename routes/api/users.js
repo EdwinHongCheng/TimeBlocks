c7c40428bcc34c0f22240df90435b17d764863e1
bcdguz <bcdguz96@gmail.com>
2021-02-22 11:39:38 -0800

    Finishing backend user auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -20,6 +20,12 @@ router.get('/current', passport.authenticate('jwt', { session: false }), (req, r
 })
 
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     User.findOne({ email: req.body.email })
         .then(user => {
             if (user) {
@@ -56,6 +62,12 @@ router.post('/register', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     const email = req.body.email;
     const password = req.body.password;
 
--- validation/login.js
+++ validation/login.js
@@ -6,31 +6,17 @@ module.exports = function validateLoginInput(data) {
 
     data.email = validText(data.email) ? data.email : '';
     data.password = validText(data.password) ? data.password : '';
-    data.name = validText(data.name) ? data.name : '';
-    data.username = validText(data.username) ? data.username : '';
 
     if (!Validator.isEmail(data.email)) {
         errors.email = 'Email is invalid';
     }
 
     if (Validator.isEmpty(data.email)) {
-        errors.email = 'Please enter an email';
+        errors.email = 'Email field is required';
     }
 
     if (Validator.isEmpty(data.password)) {
-        errors.password = 'Please enter a password';
-    }
-
-    if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-        errors.password = 'Password must be at least 6 characters';
-    }
-
-    if (Validator.isEmpty(data.name)) {
-        errors.name = 'Please input a name';
-    }
-
-    if (Validator.isEmpty(data.username)) {
-        errors.username = 'Please input a username';
+        errors.password = 'Password field is required';
     }
 
     return {
