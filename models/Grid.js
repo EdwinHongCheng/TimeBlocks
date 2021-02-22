30912a30843e02dd18fea9ac292344de93e9ee44
IsoVoyd <dubnersamuel@gmail.com>
2021-02-22 15:09:33 -0800

    fix auth issues

--- models/Grid.js
+++ models/Grid.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const GridSchema = new Schema({
-    hours: {
-        1: null, 2: null, 3: null, 4: null,
-        5: null, 6: null, 7: null, 8: null,
-        9: null, 10: null, 11:null, 12: null,
-        13: null, 14: null, 15: null, 16: null,
-        17: null, 18: null, 19: null, 20: null,
-        21: null, 22: null, 23: null, 24: null
-    },
+    // hours: {
+    //     1: null, 2: null, 3: null, 4: null,
+    //     5: null, 6: null, 7: null, 8: null,
+    //     9: null, 10: null, 11:null, 12: null,
+    //     13: null, 14: null, 15: null, 16: null,
+    //     17: null, 18: null, 19: null, 20: null,
+    //     21: null, 22: null, 23: null, 24: null
+    // },
     user: {
         type: Schema.Types.ObjectId,
         ref: 'users'
