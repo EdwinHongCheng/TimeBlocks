fea33d56c33d55371e61009fafa3f5633808f056
bcdguz <bcdguz96@gmail.com>
2021-02-22 11:13:58 -0800

    Working on user login and register routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,20 +2,20 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
 router.post('/register', (req, res) => {
-    // Check to make sure nobody has already registered with a duplicate email
     User.findOne({ email: req.body.email })
         .then(user => {
             if (user) {
-                // Throw a 400 error if the email address already exists
                 return res.status(400).json({ email: "Email address already exists!" })
             } else {
-                // Otherwise create a new user
                 const newUser = new User({
-                    handle: req.body.handle,
+                    name: req.body.name,
+                    username: req.body.username,
                     email: req.body.email,
                     password: req.body.password
                 })
@@ -24,12 +24,54 @@ router.post('/register', (req, res) => {
                     bcrypt.hash(newUser.password, salt, (err, hash) => {
                         if (err) throw err;
                         newUser.password = hash;
-                        newUser.save()
-                            .then(user => res.json(user))
+                        newUser
+                            .save()
+                            .then(user => {
+                                const payload = { id: user.id, name: user.name };
+
+                                jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                                    res.json({
+                                        success: true,
+                                        token: "Bearer " + token
+                                    });
+                                });
+                            })
                             .catch(err => console.log(err));
-                    })
-                })
+                    });
+                });
             }
+        })
+})
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({ email: 'User with this email does not exist' });
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        const payload = { id: user.id, name: user.name };
+
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            { expiresIn: 3600 },
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: 'Bearer ' + token
+                                });
+                            });
+                    } else {
+                        return res.status(400).json({ password: 'Incorrect password' });
+                    }
+                })
         })
 })
 
