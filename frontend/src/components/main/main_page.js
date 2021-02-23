ca0e1dfa220124221f6f5ce13c03b683636c3689
Edwin Cheng <edwincheng3@gmail.com>
2021-02-23 11:43:38 -0800

    Refactor Login-Signup Components

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,10 +1,14 @@
-import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
+import FirstButtonsContainer from "../first_buttons/first_buttons_container";
 
 class MainPage extends React.Component {
     render() {
         return (
-          <div>
-            <h1>TimeBlocks</h1>
+          <div className="login-signup-background">
+            <Link className="app-name" to={"/"}>TimeBlocks</Link>
+            <h1 className="logo-image">LOGO</h1>
+            <FirstButtonsContainer />
           </div>
         );
     }
