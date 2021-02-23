f898606de9b7b5cab330ef903880ca6be4977d5e
IsoVoyd <dubnersamuel@gmail.com>
2021-02-23 10:48:31 -0800

    add grid component and basic css structure

---
+++ frontend/src/components/grid/grid.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const Grid = (props) => (
+    <div className="grid-container">
+
+    </div>
+)
+
+export default Grid;
--- frontend/src/components/home/home_page.js
+++ frontend/src/components/home/home_page.js
@@ -1,9 +1,11 @@
 import React from "react"
+import Grid from "../grid/grid";
 
 class MainPage extends React.Component {
     render() {
         return (
           <div>
+            <Grid />
           </div>
         );
     }
