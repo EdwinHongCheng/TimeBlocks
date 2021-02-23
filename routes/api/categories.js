80fca9c386f193b70a82560c632242e34cd9a4e9
Michelle Ha <michelle1992us@yahoo.com>
2021-02-23 11:57:56 -0800

    add taks to categories

--- routes/api/categories.js
+++ routes/api/categories.js
@@ -6,32 +6,32 @@ const passport = require('passport');
 const Category = require('../../models/Category')
 const validateCategoryInput = require('../../validation/categories');
 
-//Get all categories (default categories?)
-router.get('/', (req, res) => {
-    Category.find()
-        .sort({ date: -1 }) //sort by most recent first
-        .then(categories => res.json(categories))
-        .catch(err => res.status(404).json({ nocategoriesfound: 'No categories found' }));
-});
+// //Get all categories (default categories?)
+// router.get('/', (req, res) => {
+//     Category.find()
+//         .sort({ date: -1 }) //sort by most recent first
+//         .then(categories => res.json(categories))
+//         .catch(err => res.status(404).json({ nocategoriesfound: 'No categories found' }));
+// });
 
-//Get all categories from a specific user
-router.get('/user/:user_id', (req, res) => {
-    Category.find({user: req.params.user_id})
-        .then(categories => res.json(categories))
-        .catch(err =>
-            res.status(404).json({ nocategoriesfound: 'No categories found from that user' }
-        )
-    );
-});
+// //Get all categories from a specific user
+// router.get('/user/:user_id', (req, res) => {
+//     Category.find({user: req.params.user_id})
+//         .then(categories => res.json(categories))
+//         .catch(err =>
+//             res.status(404).json({ nocategoriesfound: 'No categories found from that user' }
+//         )
+//     );
+// });
 
-//Get a specific category (needed?)
-router.get('/:id', (req, res) => {
-    Category.findById(req.params.id)
-        .then(category => res.json(category))
-        .catch(err =>
-            res.status(404).json({ nocategoryfound: 'No category found with that ID' })
-        );
-});
+// //Get a specific category (needed?)
+// router.get('/:id', (req, res) => {
+//     Category.findById(req.params.id)
+//         .then(category => res.json(category))
+//         .catch(err =>
+//             res.status(404).json({ nocategoryfound: 'No category found with that ID' })
+//         );
+// });
 
 //Protected route to create categories
 router.post('/',
@@ -50,15 +50,6 @@ router.post('/',
 
     newCategory.save()
         .then((cat) => res.json(cat));
-
-    //   User.findById(req.user.id).then(user => {
-    //     const newCategory = {
-    //         text: req.body.text,
-    //         // user: req.user.id
-    //     };
-    //     user.categories.push(newCategory);
-    //     user.save().then(() => res.end());
-    //   })
     }
 );
 
@@ -72,8 +63,6 @@ router.delete('/:id',
                 .catch((err) => res.status(404).json({removed: false}))
         })
 });
-
-//Edit categories un-needed?
 
 
 module.exports = router
--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -1,27 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const Task = require('../../models/Task');
+const passport = require('passport');
+const Category = require('../../models/Category')
 
 // router.get("/test", (req, res) => res.json({ msg: "This is the tasks route" }));
 
-// router.post('/',
-//     passport.authenticate('jwt', { session: false }),
-//     (req, res) => {
-//       const { errors, isValid } = validateCategoryInput(req.body);
+router.post('/',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+    //   const { errors, isValid } = validateCategoryInput(req.body);
   
-//       if (!isValid) {
-//         return res.status(400).json(errors);
-//       }
-//       User.findById(req.user.id).then(user => {
-//         const newCategory = {
-//             text: req.body.text,
-//         };
-//         user.categories.push(newCategory);
-//         user.save().then(() => res.end());
-//       })
-//       return;
-//     }
-// );
+    //   if (!isValid) {
+    //     return res.status(400).json(errors);
+    //   }
+    Category.findById(req.body.catId).then(category => {
+        const newTask = {
+            title: req.body.title,
+        };
+        category.tasks.push(newTask);
+        category.save()
+            .then((category) => res.json(category))
+            .catch(errors => res.json(errors))
+      })
+    }
+);
 
 // router.delete('/:id',
 //     passport.authenticate('jwt', { session: false }),
