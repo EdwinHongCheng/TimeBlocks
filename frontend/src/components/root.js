9e9170489959921fe10e5b1b59e5939816c9b547
IsoVoyd <dubnersamuel@gmail.com>
2021-02-24 10:49:51 -0800

    add useDrag hook to tasks

--- frontend/src/components/root.js
+++ frontend/src/components/root.js
@@ -2,12 +2,17 @@ import React from "react";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 
+import { DndProvider } from "react-dnd-multi-backend"
+import HTML5toTouch from "react-dnd-multi-backend/dist/esm/HTML5toTouch"
+
 import App from "./app";
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
-      <App />
+      <DndProvider options={HTML5toTouch}>
+        <App />
+      </DndProvider>
     </HashRouter>
   </Provider>
 );
--- frontend/src/components/task/task.js
+++ frontend/src/components/task/task.js
@@ -1,8 +1,16 @@
 import React from "react"
+import { useDrag } from "react-dnd"
 
 const Task = (props) => {
+    const [{ isDragging }, drag] = useDrag(() => ({
+        item: {type: "TASK"},
+        collect: (monitor) => ({
+            isDragging: !!monitor.isDragging()
+        })
+    }))
+
     return (
-        <div className="task">
+        <div ref={drag} className="task">
 
         </div>
     )
