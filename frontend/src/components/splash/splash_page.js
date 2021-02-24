1afbf61a580245dde203699733910af10a619fe8
Edwin Cheng <edwincheng3@gmail.com>
2021-02-24 12:22:59 -0800

    splash page - work on logo stuff

--- frontend/src/components/splash/splash_page.js
+++ frontend/src/components/splash/splash_page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import FirstButtonsContainer from "../first_buttons/first_buttons_container";
+import TimeBlocksLogo from "../../time-logo.png";
 
 class SplashPage extends React.Component {
   render() {
@@ -9,9 +10,15 @@ class SplashPage extends React.Component {
         <Link className="app-name" to={"/"}>
           TimeBlocks
         </Link>
-        <p className="splash-blurb">
-          A touch-friendly time management app for keeping track of daily tasks!
-        </p>
+        <div className="splash-body">
+          <p className="splash-blurb">
+            A touch-friendly time management app for keeping track of daily tasks!
+          </p>
+
+          {/* [ISSUE]: doesn't have it's own row */}
+          {/* <img className="logo" src={TimeBlocksLogo} /> */}
+
+        </div>
         <FirstButtonsContainer />
       </div>
     );
