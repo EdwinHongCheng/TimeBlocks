f22afcadcbcdec0c4708492ee926021dd3498f56
IsoVoyd <dubnersamuel@gmail.com>
2021-02-25 11:37:12 -0800

    add task list and task container to home page

--- frontend/src/components/home/home_page.js
+++ frontend/src/components/home/home_page.js
@@ -1,19 +1,20 @@
-import React from "react"
+import React from "react";
 import Grid from "../grid/grid";
-import Task from "../task/task"
+
+import TaskCategoriesContainer from "../task_categories/task_categories_container";
+import TaskListContainer from "../task_list/task_list_container";
 
 import LogoutButtonContainer from "../logout/logout_button_container";
 
 class HomePage extends React.Component {
-    render() {
-        return (
-          <div className="home-container">
-            <Grid />
-            <Task color={"#ff0000"}/>
-            <LogoutButtonContainer />
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div className="home-container">
+        <Grid />
+        <LogoutButtonContainer />
+      </div>
+    );
+  }
 }
 
 export default HomePage;
