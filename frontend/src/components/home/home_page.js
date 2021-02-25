c7bf16a19ec7278bc704083998228d465cdecb39
IsoVoyd <dubnersamuel@gmail.com>
2021-02-25 15:54:56 -0800

    add open task list button

--- frontend/src/components/home/home_page.js
+++ frontend/src/components/home/home_page.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Grid from "../grid/grid";
-import CategoryContainer from "../task_categories/task_categories_container"
 
 import TaskCategoriesContainer from "../task_categories/task_categories_container";
 import TaskListContainer from "../task_list/task_list_container";
@@ -8,10 +7,22 @@ import TaskListContainer from "../task_list/task_list_container";
 import LogoutButtonContainer from "../logout/logout_button_container";
 
 class HomePage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.openTaskList = this.openTaskList.bind(this)
+  }
+
+  openTaskList(e) {
+
+  }
+
   render() {
     return (
       <div className="home-container">
+        {/* <TaskCategoriesContainer /> */}
         <TaskListContainer />
+
+        <input type="button" onClick={this.openTaskList} />
         <Grid />
         <LogoutButtonContainer />
       </div>
