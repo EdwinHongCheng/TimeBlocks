007bc531f2974611360967d6984eb3298b44ebc4
Michelle Ha <michelle1992us@yahoo.com>
2021-02-25 13:37:55 -0800

    compose new categories

--- frontend/src/components/task_categories/category_box.js
+++ frontend/src/components/task_categories/category_box.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 class CategoryBox extends React.Component {
-  
+  //change "text" to "title" when bryce pushes up edited validations
   render() {
     return (
-        <div>
-            <h3>{this.props.text}</h3>
+        <div> 
+            <h3>{this.props.text}</h3> 
             <ul>
               <h3>Tasks: </h3>
               <li>{this.props.tasks.title}</li>
