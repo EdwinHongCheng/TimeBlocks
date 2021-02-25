5838fa17163945645e73dd0132278ce916c5635c
Michelle Ha <michelle1992us@yahoo.com>
2021-02-25 14:06:04 -0800

    fix rendering categories

--- frontend/src/components/task_categories/category_box.js
+++ frontend/src/components/task_categories/category_box.js
@@ -1,18 +1,24 @@
 import React from 'react';
 
 class CategoryBox extends React.Component {
+ 
   //change "text" to "title" when bryce pushes up edited validations
   render() {
+    let task = this.props.tasks.map(task => 
+      {
+      return (<li>{task.title}</li>)
+    })
+    
     return (
         <div> 
             <h3>{this.props.title}</h3> 
             <ul>
-              <h3>Tasks: </h3>
-              <li>{this.props.tasks.title}</li>
+              <h6>Tasks: </h6>
+              {task}
             </ul>
         </div>
     );
-  }
-}
+  };
+};
 
 export default CategoryBox;
