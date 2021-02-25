97ff3f6b7ba21d7b23ca9fa34dea645fac12aaf5
bcdguz <bcdguz96@gmail.com>
2021-02-25 11:20:31 -0800

    Created method to seed user on registration

--- util/seed.js
+++ util/seed.js
@@ -2,16 +2,34 @@ const Category = require('../models/Category');
 
 
 const seedUser = (user) => {
-    const catSeed1 = {
+    const catSeed1 = new Category({
         text: "Health and Fitness",
         userId: user.id,
         color: "#FF5733"
-    }
-    const catSeed2 = {
+    });
+    const catSeed2 = new Category({
         text: "Chores",
         userId: user.id,
         color: "#33FFEC"
-    }
+    });
+
+    catSeed1.save()
+        .then(catSeed => {
+            const task1 = { title: "30 min Run" };
+            const task2 = { title: "Morning Yoga" };
+            catSeed.tasks.push(task1);
+            catSeed.tasks.push(task2);
+            catSeed.save();
+        })
+    
+    catSeed2.save()
+        .then(catSeed => {
+            const task1 = { title: "Clean the Dishes" };
+            const task2 = { title: "Take out the trash" };
+            catSeed.tasks.push(task1);
+            catSeed.tasks.push(task2);
+            catSeed.save();
+        })
 }
 
 module.exports = seedUser;
