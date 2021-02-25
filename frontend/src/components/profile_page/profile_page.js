c321717a1ebe4339d96c176a3671ed26f23d3fe9
Edwin Cheng <edwincheng3@gmail.com>
2021-02-25 00:36:47 -0800

    Work on Profile Page skeleton

--- frontend/src/components/profile_page/profile_page.js
+++ frontend/src/components/profile_page/profile_page.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import LogoutButton from "../logout/logout_button_container";
 
 class ProfilePage extends React.Component {
@@ -6,6 +7,8 @@ class ProfilePage extends React.Component {
         return (
             <div className="profile-page-background">
                 <p>Profile Page</p>
+                <Link to="/home">Back to Home</Link>
+                <p>Username (Placeholder)</p>
                 <LogoutButton />
             </div>
         )
