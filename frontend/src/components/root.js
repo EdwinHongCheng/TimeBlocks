fd7fb1ef3685ad95afbe62eb63b24e263ee547f0
IsoVoyd <dubnersamuel@gmail.com>
2021-02-24 18:02:43 -0800

    fix drag not transitioning to touch backend

--- frontend/src/components/root.js
+++ frontend/src/components/root.js
@@ -3,14 +3,29 @@ import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 
 import { DndProvider } from "react-dnd-multi-backend"
-import HTML5toTouch from "react-dnd-multi-backend/dist/esm/HTML5toTouch"
+import {HTML5Backend} from "react-dnd-html5-backend";
+import {TouchBackend} from "react-dnd-touch-backend";
+import MultiBackend, { TouchTransition } from "react-dnd-multi-backend";
 
 import App from "./app";
+
+const HTML5toTouch = {
+  backends: [
+    {
+      backend: HTML5Backend
+    },
+    {
+      backend: TouchBackend,
+      preview: true,
+      transition: TouchTransition
+    }
+  ]
+}
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
-      <DndProvider options={HTML5toTouch}>
+      <DndProvider backend={MultiBackend} options={HTML5toTouch}>
         <App />
       </DndProvider>
     </HashRouter>
