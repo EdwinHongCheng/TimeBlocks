c0f84c814ded6256e202b5144c28204665245530
Edwin Cheng <edwincheng3@gmail.com>
2021-02-25 10:42:41 -0800

    add profile page buttons

--- frontend/src/components/profile_page/profile_page.js
+++ frontend/src/components/profile_page/profile_page.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import LogoutButton from "../logout/logout_button_container";
 
 class ProfilePage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.logoutUser = this.logoutUser.bind(this);
+    }
+
+    logoutUser(e) {
+        e.preventDefault();
+        this.props.logout();
+        return (<Link to={"/"}/>)
+    }
+
     render() {
         return (
             <div className="profile-page-background">
+                
                 <div className="profile-header-parent">
                     <p className="profile-header">User Profile</p>
                 </div>
@@ -13,8 +25,15 @@ class ProfilePage extends React.Component {
                 <div className="prof-page-body">
                     <p className="user-name">John Wick</p>
                     <p className="friends-list">Friends List (3)</p>
-                    <p>Clear Grid Button</p>
-                    <LogoutButton />
+
+                    <div className="profile-page-buttons">
+                        <p className="clear-grid-button" onClick={() => console.log("hello")}>
+                            Clear Grid
+                        </p>
+                        <p className="logout-button" onClick={this.logoutUser}>
+                            Log Out
+                        </p>
+                    </div>
                 </div>
 
                 <Link className="back-to-home-button" to="/home">Back</Link>
--- frontend/src/components/profile_page/profile_page_container.js
+++ frontend/src/components/profile_page/profile_page_container.js
@@ -1,16 +1,15 @@
 import { connect } from "react-redux";
 import ProfilePage from "./profile_page";
+import { logout } from "../../actions/session_actions";
 
 const mapStateToProps = (state) => {
     return {
-
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ProfilePage);
+export default connect(mapStateToProps, {logout})(ProfilePage);
