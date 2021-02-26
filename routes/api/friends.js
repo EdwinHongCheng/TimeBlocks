8d1b318ee450021b72cf4244f2d7f8f020e9bccf
bcdguz <bcdguz96@gmail.com>
2021-02-25 17:09:33 -0800

    Working on add friend route

--- routes/api/friends.js
+++ routes/api/friends.js
@@ -2,3 +2,19 @@ const express = require("express");
 const router = express.Router();
 const passport = require('passport');
 const User = require('../../models/User');
+
+//add friend
+router.post('/', passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        User.findById(req.user.id)
+            .then(user => {
+                user.friends.push(req.body.userId);
+                user.save()
+                    .then(user => res.json(user))
+                    .catch(() => res.json({error: "User Id not found!"}))
+            })
+            
+    }
+);
+
+module.exports = router;
