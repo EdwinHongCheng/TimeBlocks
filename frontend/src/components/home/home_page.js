2034154096fabbe4040f7dd784611e08d1694bcf
IsoVoyd <dubnersamuel@gmail.com>
2021-02-25 16:35:00 -0800

    import Link on home page

--- frontend/src/components/home/home_page.js
+++ frontend/src/components/home/home_page.js
@@ -2,6 +2,7 @@ import React from "react";
 import Grid from "../grid/grid";
 // import CategoryContainer from "../task_categories/task_categories_container"
 // import Menu from "../sliding_menu/sliding_pane"
+import { Link } from "react-router-dom"
 
 import TaskCategoriesContainer from "../task_categories/task_categories_container";
 import TaskListContainer from "../task_list/task_list_container";
@@ -24,7 +25,10 @@ class HomePage extends React.Component {
         {/* <TaskCategoriesContainer /> */}
         {/* <TaskListContainer /> */}
 
-        <input type="button" onClick={this.openTaskList} />
+        <div>
+          <input type="button" onClick={this.openCategories} value="Show Categories" />
+          <input type="button" onClick={this.openTaskList} value="Show Task List" />
+        </div>
         <Grid />
         {/* [TEST][WORKS] Profile Page */}
         <Link to="/profile-page">Profile Page</Link>
