0dcdd977e12da8880e3c23b2bb335e20f9c8aa07
Michelle Ha <michelle1992us@yahoo.com>
2021-02-26 11:08:47 -0800

    tasks reducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,11 +7,6 @@ import configureStore from "./store/store";
 
 import { setAuthToken } from "./util/session_api_util";
 import { logout } from "./actions/session_actions"; 
-////TEST////
-import axios from 'axios'
-// import {fetchCategories, newCategory, destroyCategory } from "./actions/category_actions"
-import {newTask, destroyTask, updateTaskTitle, updateTaskCategory} from "./actions/task_actions"
-import {editTaskTitle} from "./util/task_api_util"
 
 document.addEventListener("DOMContentLoaded", () => {
   let store;
@@ -38,14 +33,6 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   const root = document.getElementById("root");
-//TEST//
-  window.store = store
-window.axios = axios
-  window.newTask = newTask
-  window.destroyTask = destroyTask
-  window.updateTaskTitle = updateTaskTitle
-  window.updateTaskCategory = updateTaskCategory
-  window.editTaskTitle = editTaskTitle
 
   ReactDOM.render(<Root store={store} />, root);
 });
---
+++ frontend/src/reducers/tasks_reducer.js
@@ -0,0 +1,22 @@
+import {RECEIVE_NEW_TASK, REMOVE_TASK, RECEIVE_TASK} from "../actions/task_actions"
+
+const TasksReducer = (oldState={}, action) => {
+    Object.freeze(oldState);
+    let nextState = Object.assign({}, oldState);
+
+    switch(action.type) {
+        case RECEIVE_NEW_TASK:
+            nextState[action.task.id] = action.task;
+            return nextState;
+        case REMOVE_TASK:
+            delete nextState[action.taskId]
+            return nextState;
+        case RECEIVE_TASK: //unsure
+            nextState[action.taskId] = action.taskId
+            return nextState
+        default:
+            return oldState;
+    }
+}
+
+export default TasksReducer
