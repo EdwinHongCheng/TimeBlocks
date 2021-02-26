024312e7e0853ad72f289249b05d0089b11c6e8c
Michelle Ha <michelle1992us@yahoo.com>
2021-02-25 17:52:44 -0800

    fix edit task category

--- frontend/src/util/task_api_util.js
+++ frontend/src/util/task_api_util.js
@@ -12,10 +12,12 @@ export const editTaskTitle = (taskId, data) => {
 
 //Edit task category
 export const editTaskCategory = (taskId, data) => {
-    return axios.put(`/api/tasks/updateCategory/${taskId}`, data)
+    return axios.post(`/api/tasks/updateCategory/${taskId}`, data)
 }
 
 //Delete task
 export const deleteTask = taskId => {
     return axios.delete(`/api/tasks/${taskId}`)
 }
+
+//TESTED. Works.
