a721abc1a627e07d667e99957fd10818675224e6
IsoVoyd <dubnersamuel@gmail.com>
2021-02-26 09:47:34 -0800

    make categories panel use categories from state

--- frontend/src/components/task_categories/task_categories.js
+++ frontend/src/components/task_categories/task_categories.js
@@ -1,19 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import TaskCategoryItem from "./task_category_item"
 
 const TaskCategories = (props) => {
-  console.log(props.currentUser)
+  
+  const [categories, setCategories] = useState(props.categories)
 
   useEffect(() => {
-    // props.fetchCategories(props.currentUser.id)
-  })
+    props.fetchCategories(props.currentUser.id)
+  }, categories)
 
   return (
     <div className="task-categories-container">
-      {/* <TaskCategoryItem /> */}
       {props.categories.map(category => {
         return (
-          <TaskCategoryItem category={category} />
+          <TaskCategoryItem key={category._id} category={category} />
         )
       })}
     </div>
@@ -21,34 +21,3 @@ const TaskCategories = (props) => {
 }
 
 export default TaskCategories;
-// class TaskCategories extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             categories: []
-//         }
-//     }
-
-//     componentWillMount() {
-//         this.props.fetchCategories(this.props.currentUser.id)
-//     }
-
-//     componentWillReceiveProps(nextProps) {
-//         this.setState({categories: nextProps.categories})
-//     }
-
-//     render() {
-//         if (this.state.categories.length === 0) {
-//             return (<div>There are no categories</div>)
-//           } else {
-//             return (
-//               <div>
-//                 <h1>Categories:</h1>
-//                 {this.state.categories.map(category => (
-//                   <CategoryBox key={category._id} title={category.title} tasks={category.tasks} />
-//                 ))}
-//               </div>
-//             );
-//           }
-//         }
-//       }
--- frontend/src/components/task_categories/task_category_item.js
+++ frontend/src/components/task_categories/task_category_item.js
@@ -5,42 +5,18 @@ const TaskCategoryItem = (props) => {
     return (
         <div className="category-container">
             <div className="category-info">
-                <h1>Category Name</h1>
-                <h2>3</h2>
+                <h1>{props.category.text}</h1>
+                <h2>{props.category.tasks.length}</h2>
             </div>
             <div className="category-tasks">
-                <Task color="#ff0000"></Task>
-                <Task color="#00ff00"></Task>
-                <Task color="#0000ff"></Task>
+                {props.category.tasks.forEach(task => {
+                    return (
+                        <Task color="#ff0000"></Task>
+                    )
+                })}
             </div>
         </div>
     )
 }
 
 export default TaskCategoryItem;
-
-// class CategoryBox extends React.Component {
-
-//   //change "text" to "title" when bryce pushes up edited validations
-//   render() {
-//     let task = this.props.tasks.map(task => 
-//       {
-//       return (
-//         <li>{task.title}</li>
-//       )
-//     })
-    
-//     return (
-//         <div> 
-//             <h3>{this.props.title}</h3> 
-//             <ul>
-//               <h6>Tasks: </h6>
-//               <li>{task}</li>
-//             </ul>
-//             {/* <button onclick={this.task}>{this.props.title}</button> */}
-//         </div>
-//     );
-//   };
-// };
-
-// export default CategoryBox;
