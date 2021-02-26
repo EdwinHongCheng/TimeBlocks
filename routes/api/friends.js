acd25f9b11e8edf21d658354b7bf7ee9c367b5c6
bcdguz <bcdguz96@gmail.com>
2021-02-25 17:03:16 -0800

    Creating friends routes

---
+++ routes/api/friends.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const router = express.Router();
+const passport = require('passport');
+const User = require('../../models/User');
--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const passport = require('passport');
-const Category = require('../../models/Category')
+const Category = require('../../models/Category');
 const validateTaskInput = require('../../validation/task');
 
 //New Task
