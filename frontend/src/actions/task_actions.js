798ef6341bcbf3fcbb6af1e5e02b14218fa23613
Michelle Ha <michelle1992us@yahoo.com>
2021-02-26 09:51:42 -0800

    thunk actions for tasks

--- frontend/src/actions/task_actions.js
+++ frontend/src/actions/task_actions.js
@@ -1,15 +1,15 @@
 import {createTask, editTaskTitle, editTaskCategory, deleteTask} from "../util/task_api_util"
 
 
-export const CREATE_TASK = "CREATE_TASK"
+export const RECEIVE_NEW_TASK = "RECEIVE_NEW_TASK"
 
 export const REMOVE_TASK = "REMOVE_TASK"
 
 export const RECEIVE_TASK = 'RECEIVE_TASK';
 
 
-export const NewTask = task => ({
-    type: CREATE_TASK,
+export const receiveNewTask = task => ({
+    type: RECEIVE_NEW_TASK,
     task
 })
 
@@ -24,12 +24,30 @@ export const receiveTask = task => ({
 }) 
 
 //Create a task
-
+export const newTask = data => dispatch => (
+    createTask(data)
+        .then(task => dispatch(receiveNewTask(task)))
+        .catch(err => console.log(err))
+)
 
 //Delete a task
-
+export const destroyTask = taskId => dispatch => (
+    deleteTask(taskId)
+        .then(() => dispatch (removeTask(taskId)))
+        .catch(err => console.log(err))
+)
 
 //Edit task title
+export const updateTaskTitle = (taskId, data) => dispatch => (
+    editTaskTitle(taskId, data)
+        .then(() => dispatch(receiveTask(taskId)))
+        .catch(err => console.log(err))
+)
 
 
 //Edit task category
+export const updateTaskCategory = (taskId, data) => dispatch => (
+    editTaskCatgeory(taskId, data)
+        .then(task => dispatch(receiveNewTask(task)))
+        .catch(err => console.log(err))
+)
