4de2411d743e26ba20fd2996545e25ce2e1d539e
bcdguz <bcdguz96@gmail.com>
2021-02-28 10:59:56 -0800

    Creating form for adding a task

--- frontend/src/components/task_categories/add_task_form.js
+++ frontend/src/components/task_categories/add_task_form.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
 class TaskForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: ""
+        }
+        this.handleInput = this.handleInput.bind(this);
+    }
+
+    handleInput() {
+        
+    }
+
     render() {
         return(
             <form>
-                <input placeholder="Task Title"></input>
+                <input
+                placeholder="Task Title"
+                value={this.state.title}
+                onChange={this.handleInput}></input>
                 <button>Add Task</button>
             </form>
         )
