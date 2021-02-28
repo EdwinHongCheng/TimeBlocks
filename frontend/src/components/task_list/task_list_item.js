08fed839a14ee7f5b063fe45281aa0994321cdec
IsoVoyd <dubnersamuel@gmail.com>
2021-02-27 18:23:06 -0800

    make task list dynamically render tasks

--- frontend/src/components/task_list/task_list_item.js
+++ frontend/src/components/task_list/task_list_item.js
@@ -3,7 +3,7 @@ import React from "react"
 const TaskListItem = (props) => {
     return(
         <div className="task-list-item">
-            <h1>{props.task.name}</h1>
+            <h1>{props.task.title}</h1>
             <h1>{props.task.hour}</h1>
         </div>
     )
